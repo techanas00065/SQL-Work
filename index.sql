5d8993dfd90b6430e7f2a66e607172891c2c405c
Anas khan <162820616+techanas00065@users.noreply.github.com>
2024-04-22 12:53:56 -0700

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,12 @@
+(for create data base extension is "create database")
+
+for creat table ("creat table then table ka naam phir columns k naam us main number wali cheez k liya (int) use hota h or words k liya (varchar) use hota h ")
+
+table delet karny k liya ("drop") use hota h OR Add karny k liya ("ADD") use hota h
+
+ .table banany k baad us main data insert karny k liya ("INSERT into amazon (id, firstname, lastname, product, price  phir VALUE likhingy phir bracket main (1, asad, khan, laptop, 200);") 
+
+
+
+kisi bhi id ko search karny k liya ("Search * FROM phir table ka naaam just like "`costumer` phir WHERE id = 1"")
+id delet karny k liya bhi  ("Search * FROM phir table ka naaam just like "`costumer` phir WHERE id = 1"") asa hi hoga.
